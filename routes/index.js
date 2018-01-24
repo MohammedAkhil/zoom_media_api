cf1e26085bb0036d7049a213e614abd032380670
Akhil Mohammed <mohammed.akhil@gyanmatrix.com>
2018-01-25 00:51:28 +0530

    check for push notification

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { video: 'Express', audio: 's' });
 });
 
 
 /* GET home page. */
 router.post('/recordings', function(req, res, next) {
-    console.log(req.body);
-    console.log('Route');
+    // var meeting_id = req.body.id;
+    var status = req.body.status;
+    var type = req.body.type;
+
+    if (type === 'RECORDING_MEETING_COMPLETED') {
+        var files = req.body.content.recording_files;
+        console.log(files);
+        var mp4 = files[0].file_path;
+        var m4a = files[1].file_path;
+        console.log(mp4);
+        res.render('index', {video: mp4, audio: m4a})
+    }
+    console.log(status);
     res.sendStatus(200);
 });
 
